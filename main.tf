cc450ebbd6df38eba57eeb439bc735016ec8952a
Kari <k.sadat@gmail.com>
2021-02-17 23:08:32 -0500

    add input variables

--- main.tf
+++ main.tf
@@ -9,10 +9,20 @@ terraform {
 
 provider "aws" {
   profile = "default"
-  region  = "us-east-1"
+  region  = var.region
 }
 
 resource "aws_instance" "tfcourse" {
   ami           = "ami-00ddb0e5626798373"
   instance_type = "t2.micro"
+}
+
+resource "aws_eip" "ip" {
+  vpc      = true
+  instance = aws_instance.tfcourse.id
+}
+
+/* output the public IP address of the elastic IP address that we create*/
+output "ip" {
+  value = aws_eip.ip.public_ip
 }
